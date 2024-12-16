73be7831b588320dda845206ab406b235a319d9a
EC <p0726294@gmail.com>
2024-12-16 15:05:15 -0300

    agregue: la funcion para mostrar tareas, guardarlas y resetearlas segun el dia, colores y ya

--- pruebahora.c
+++ pruebahora.c
@@ -68,6 +68,50 @@ void actualizar_estado_tarea(const char *nombre_archivo, const char *tarea, int 
     printf("Tarea no encontrada para modificar.\n");
 }
 
+void todolistGeneral(const char *archivo_config, int opciones,const char *dia){
+	char tarea1[100];
+	snprintf(tarea1, sizeof(tarea1), "%s%s", "estudiar_C", dia);
+	char tarea2[100];
+	snprintf(tarea2, sizeof(tarea2), "%s%s", "avanzarProyecto", dia);
+	char tarea3[100];
+	snprintf(tarea3, sizeof(tarea3), "%s%s", "todolist_emocional", dia);
+
+
+
+		    	if(obtener_estado_tarea(archivo_config, tarea1) == 0){
+		    		printf("\033[33m(1):Estudiar C!\033[33m\n");
+		    	}
+
+
+		    	if(obtener_estado_tarea(archivo_config, tarea2) == 0){
+		    		printf("\033[33m(2):Avanzar Proyecto C!\033[33m\n");
+		    	}
+
+
+
+		    	if(obtener_estado_tarea(archivo_config, tarea3) == 0){
+		    		printf("\033[33m(3):Hacer Todolist Emocional !\033[33m\n");
+		    	}
+
+
+		    	printf("\033[32mselecciona tarea para marcarla como hecho... \033[32m");
+    		    	scanf("%d", &opciones);
+			if(opciones == 1){
+				actualizar_estado_tarea(archivo_config, tarea1, 1);
+			}
+
+
+			if(opciones == 2){
+				actualizar_estado_tarea(archivo_config, tarea2, 1);
+			}
+
+
+			if(opciones == 3){
+				actualizar_estado_tarea(archivo_config, tarea3, 1);
+			}
+
+
+}
 
 int main() {
    	bool verdadero = true; 
@@ -98,12 +142,12 @@ int main() {
                            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
     	// Mostrar la fecha y hora
-    	printf("Hoy es %s, %d de %s de %d.\n",
+    	printf("\033[32mHoy es %s, %d de %s de %d.\033[32m\n",
            	dias[local_time->tm_wday],
           	 local_time->tm_mday,
            	meses[local_time->tm_mon],
            	local_time->tm_year + 1900);
-    	printf("Hora actual: %02d:%02d:%02d\n",
+    	printf("\033[32mHora actual: %02d:%02d:%02d\033[32m\n",
            	local_time->tm_hour,
            	local_time->tm_min,
            	local_time->tm_sec);
@@ -113,65 +157,54 @@ int main() {
     while(verdadero){
      	switch(dia){
     		case DOMINGO:
-			printf("pansa");
+			printf("Descansa noob");
 			break;
      		case LUNES:                  
- 			printf("pansa");
- 			break;            
-    		case MARTES:
-			printf("\nLA POLLA GENERAL\n");
-		    	printf("toca cualquier tecla para apagar: ");
-		    	break;            
-     		case MIERCOLES:                  
- 			printf("pansa");
- 		    	printf("toca cualquier tecla para apagar: ");
+			todolistGeneral(archivo_config, opciones,"1");			
 			break;            
-    		case JUEVES:
-			printf("pansa");  	    
-		    	printf("toca cualquier tecla para apagar: ");
+    		case MARTES:
+			actualizar_estado_tarea(archivo_config, "estudiar_C1", 0);
+			actualizar_estado_tarea(archivo_config, "avanzarProyecto1", 0);
+			actualizar_estado_tarea(archivo_config, "todolist_emocional1", 0);
+
+	
+			todolistGeneral(archivo_config, opciones, "2");			
+		    	break;            
+		case MIERCOLES:
+			actualizar_estado_tarea(archivo_config, "estudiar_C2", 0);
+			actualizar_estado_tarea(archivo_config, "avanzarProyecto2", 0);
+			actualizar_estado_tarea(archivo_config, "todolist_emocional2", 0);
+
+
+			todolistGeneral(archivo_config, opciones, "3");			
+			break;            
+		case JUEVES:
+			actualizar_estado_tarea(archivo_config, "estudiar_C3", 0);
+			actualizar_estado_tarea(archivo_config, "avanzarProyecto3", 0);
+			actualizar_estado_tarea(archivo_config, "todolist_emocional3", 0);
+
+
+			todolistGeneral(archivo_config, opciones, "4");			
 			break;            	
 	
-    		case VIERNES:
-		    	if(obtener_estado_tarea(archivo_config, "estudiar_C") == 0){
-		    		printf("(1):Estudiar C!\n");
-		    	}
+		case VIERNES:
+			actualizar_estado_tarea(archivo_config, "estudiar_C4", 0);
+			actualizar_estado_tarea(archivo_config, "avanzarProyecto4", 0);
+			actualizar_estado_tarea(archivo_config, "todolist_emocional4", 0);
 
-		    	if(obtener_estado_tarea(archivo_config, "hacer_ejercicio") == 0){
-		    		printf("(2):Hacer brazo !\n");
-		    	}
-
-		    	if(obtener_estado_tarea(archivo_config, "avanzarProyecto") == 0){
-		    		printf("(3):Avanzar Proyecto C!\n");
-		    	}
-
-		    	printf("selecciona tarea para marcarla como hecho... ");
-    		    	scanf("%d", &opciones);
-			if(opciones == 1){
-				actualizar_estado_tarea(archivo_config, "estudiar_C", 1);
-			}
-
-			else if(opciones == 2){
-				actualizar_estado_tarea(archivo_config, "hacer_ejercicio", 1);
-			}
-
-			if(opciones == 3){
-				actualizar_estado_tarea(archivo_config, "avanzarProyecto", 1);
-			}
-
-
+			todolistGeneral(archivo_config, opciones, "5");			
 			break;            
     		case SABADO:
-	  		actualizar_estado_tarea(archivo_config, "estudiar_C", 0);
-			
-	  		actualizar_estado_tarea(archivo_config, "hacer_ejercicio", 0);		    	
-	  		actualizar_estado_tarea(archivo_config, "avanzarProyecto", 0);
-			//cuando pase de viernes a domingo, las tareas anteriores volveran a estar
-			//visibles.
-			printf("toca cualquier tecla para apagar: ");
+			actualizar_estado_tarea(archivo_config, "estudiar_C5", 0);
+			actualizar_estado_tarea(archivo_config, "avanzarProyecto5", 0);
+			actualizar_estado_tarea(archivo_config, "todolist_emocional5", 0);
+
+
+			printf("descansa noob"); 	
 			break;            
 
     	}
-	printf("Desea Salir?. si(1), no(0)");
+	printf("\033[32mDesea Salir?. si(1), no(0)\033[32m");
 	scanf("%d",&salida);
   	if(salida == 1){
 		break;
